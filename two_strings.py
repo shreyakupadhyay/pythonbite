8e4c5dbee81b7a19fb4d0146099e240be2533dc8
Shreyak Upadhyay <shreyakupadhyay07@gmail.com>
2016-10-25 01:28:18 +0530

    Update two_strings.py

--- two_strings.py
+++ two_strings.py
@@ -1,35 +1,16 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
 test_case=input()
-for i in range(0,test_case):
+
+for num in range(0,test_case):
     first=raw_input()
     second=raw_input()
-    length_first=len(first)
-    length_second=len(second)
-    #if(length_first<=length_second):
+    len_first=len(first)
+    len_second=len(second)
     counter=0
-    for j in range(0,26):
-        #print chr(ord('a')+j)
-    	if(chr(ord('a')+j) in first and chr(ord('a')+j) in second):
-    		counter=1
-    		break
+    for alpha_val in range(0,26):
+        if(chr(ord('a')+alpha_val) in first and chr(ord('a')+alpha_val) in second):
+            counter=1
+            break
     if(counter==1):
         print "YES"
     if(counter==0):
         print "NO"
-   		#if(chr(ord('a')+i) in first and chr(ord('a')+i) in second):
-   		#	counter=1
-   		#	break
-           #if(counter==0):
-   		#print "NO"
-   	#if(counter==1):
-   	#	print "YES"
-    #if(length_first>length_second):
-    #	counter=0
-    #	for i in range(0,length_second):
-    #		if(second[i] in first):
-    #			counter=counter+1
-    #			continue
-    #	if(counter>0):
-    #		print "YES"
-    #	if(counter==0):
-    #		print "NO"
